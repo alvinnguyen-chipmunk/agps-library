59f940bb29f340daa8eb5acb6c9b33d2fba69f80
Luck Hoang <luck.hoang@styl.solutions>
2017-06-15 16:12:58 +0700

    build-stylagps.sh: add static build
    Signed-off-by: Luck Hoang <luck.hoang@styl.solutions>

--- build-stylagps.sh
+++ build-stylagps.sh
@@ -23,3 +23,13 @@ cp ../stylagps.conf etc/stylagps
 tar cJf ${TARGET} ./*
 mv ${TARGET} ..
 cd ${BUILD}
+
+function build_static()
+{
+	$CC -Wall -c stylAgpsUtils.c stylagps.c $(pkg-config --libs --cflags glib-2.0 libnm json-c libcurl) -I. -L.
+	$AR rcs libstylagps.a stylagps.o stylAgpsUtils.o
+
+	$CC -o stylagps_demo_static examples/main.c -L/data/linux-workspace/styl/project/busterminal/stylagps/ -lstylagps $(pkg-config --cflags --libs json-c libnm glib-2.0 libcurl)
+}
+
+build_static
